d3387a538da795021de154e5f871f04931dae585
BitStellarius <stellariusgaming@gmail.com>
2021-05-05 10:31:43 +0200

    SoundFX hinzugefügt zu Sektion Welt Buch Sektion

--- js/sounds.js
+++ js/sounds.js
@@ -21,6 +21,46 @@ const ttrpgBtn2 = document.getElementsByClassName("questionBox")[1];
 const ttrpgBtn3 = document.getElementsByClassName("questionBox")[2];
 const ttrpgBtn4 = document.getElementsByClassName("questionBox")[3];
 
+// Welt
+const indexRassenBtn = document.getElementById("indexRassen");
+const indexReligionBtn = document.getElementById("indexReligion");
+const indexMagieBtn = document.getElementById("indexMagie");
+const orksBtn = document.getElementById("orks");
+const elbenBtn = document.getElementById("elben");
+const halbElbenBtn = document.getElementById("halbElben");
+const halblingBtn = document.getElementById("halbling");
+const zwergeBtn = document.getElementById("zwerge");
+const katzenmenschenMagieBtn = document.getElementById("katzenmenschen");
+const mischwesenBtn = document.getElementById("mischwesen");
+const thyrosBtn = document.getElementById("thyros");
+const arisBtn = document.getElementById("aris");
+const begrendBtn = document.getElementById("begrend");
+const arphenBtn = document.getElementById("arphen");
+const trendirBtn = document.getElementById("trendir");
+const tradosBtn = document.getElementById("trados");
+const koshkaBtn = document.getElementById("koshka");
+const seonoraBtn = document.getElementById("seonora");
+const ferrumtisBtn = document.getElementById("ferrumtis");
+const fanuinBtn = document.getElementById("fanuin");
+const varrakisBtn = document.getElementById("varrakis");
+const danduleinBtn = document.getElementById("dandulein");
+const elementBtn = document.getElementById("element");
+const naturBtn = document.getElementById("natur");
+const schutzBtn = document.getElementById("schutz");
+const beschwoerBtn = document.getElementById("beschwoer");
+const illusionBtn = document.getElementById("illusion");
+const wiederherBtn = document.getElementById("wiederher");
+const bannmagieBtn = document.getElementById("bannmagie");
+const fortbewegungBtn = document.getElementById("fortbewegung");
+const wissensBtn = document.getElementById("wissens");
+const veraenderungBtn = document.getElementById("veraenderung");
+const dunkleBtn = document.getElementById("dunkle");
+const infoIconBtn = document.getElementById("infoIcon");
+const rassenIconBtn = document.getElementById("rassenIcon");
+const religionIconBtn = document.getElementById("religionIcon");
+const magieIconBtn = document.getElementById("magieIcon");
+
+
 /* Event Listener */
 
 //TTRPG
@@ -34,5 +74,119 @@ ttrpgBtn3.addEventListener("click", function(){
   pageFlip[rng(3)].play();
 });
 ttrpgBtn4.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+
+//Welt
+indexRassenBtn.addEventListener("click", function(){
+  chapterFlip.play();
+});
+indexReligionBtn.addEventListener("click", function(){
+  chapterFlip.play();
+});
+indexMagieBtn.addEventListener("click", function(){
+  chapterFlip.play();
+});
+infoIconBtn.addEventListener("click", function(){
+  chapterFlip.play();
+});
+rassenIconBtn.addEventListener("click", function(){
+  chapterFlip.play();
+});
+religionIconBtn.addEventListener("click", function(){
+  chapterFlip.play();
+});
+magieIconBtn.addEventListener("click", function(){
+  chapterFlip.play();
+});
+
+orksBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+elbenBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+halbElbenBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+halblingBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+zwergeBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+katzenmenschenMagieBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+mischwesenBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+thyrosBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+arisBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+begrendBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+arphenBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+trendirBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+tradosBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+koshkaBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+seonoraBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+ferrumtisBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+fanuinBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+varrakisBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+danduleinBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+elementBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+naturBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+schutzBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+beschwoerBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+illusionBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+wiederherBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+bannmagieBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+fortbewegungBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+wissensBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+veraenderungBtn.addEventListener("click", function(){
+  pageFlip[rng(3)].play();
+});
+dunkleBtn.addEventListener("click", function(){
   pageFlip[rng(3)].play();
 });
