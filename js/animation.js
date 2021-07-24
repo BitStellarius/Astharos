e716fa27562e84a6c773d4ec865aa516f907c233
BitStellarius <stellariusgaming@gmail.com>
2021-07-24 19:11:05 +0200

    Karte Animationen

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,5 @@
 /* Animationen */
-
+/*
 // Start: Deco Frame links
 let animFrameDecoLeft = anime({
   targets: '#startframeDecoLeft',
@@ -17,6 +17,7 @@ function playAnimLeft() {
 }
 
 document.getElementById('preloaderButton').addEventListener("click", playAnimLeft);
+*/
 
 // Welt: Ketten
 let triggerCheck = 0;
@@ -42,3 +43,133 @@ var kettenWP = new Waypoint({
   },
   offset: -200
 });
+
+// Karte Deco Frames Links
+
+let animAsthosDecoLeft = anime({
+  targets: '#asthosMauerLinks',
+  translateX: [
+    {value: 990, duration: 1500},
+  ],
+  easing: 'easeOutSine',
+  loop: false,
+  autoplay: false
+});
+
+function animAsthosLinks() {
+  animAsthosDecoLeft.play();
+}
+
+document.getElementById('iconAsthos').addEventListener("click", animAsthosLinks);
+
+//Flostapor
+
+let animFlostaporDecoLinks = anime({
+  targets: '#flostaporMauerLinks',
+  translateX: [
+    {value: 1000, duration: 1500},
+  ],
+  easing: 'easeOutSine',
+  loop: false,
+  autoplay: false
+});
+
+function animFlostaporLinks() {
+  animFlostaporDecoLinks.play();
+}
+
+document.getElementById('iconFlostapor').addEventListener("click", animFlostaporLinks);
+
+
+
+//Glarius
+
+let animGlariusDecoLinks = anime({
+  targets: '#glariusMauerLinks',
+  translateX: [
+    {value: 1000, duration: 1500},
+  ],
+  easing: 'easeOutSine',
+  loop: false,
+  autoplay: false
+});
+
+function animGlariusLinks() {
+  animGlariusDecoLinks.play();
+}
+
+document.getElementById('iconGlarius').addEventListener("click", animGlariusLinks);
+
+//Herbanas
+
+let animHerbanasDecoLinks = anime({
+  targets: '#herbanasMauerLinks',
+  translateX: [
+    {value: 1000, duration: 2000},
+  ],
+  easing: 'easeOutSine',
+  loop: false,
+  autoplay: false
+});
+
+function animHerbanasLinks() {
+  animHerbanasDecoLinks.play();
+}
+
+document.getElementById('iconHerbanas').addEventListener("click", animHerbanasLinks);
+
+//Miratan
+
+let animMiratanDecoLinks = anime({
+  targets: '#miratanMauerLinks',
+  translateX: [
+    {value: 980, duration: 2000},
+  ],
+  easing: 'easeOutSine',
+  loop: false,
+  autoplay: false
+});
+
+function animMiratanLinks() {
+  animMiratanDecoLinks.play();
+}
+
+document.getElementById('iconMiratan').addEventListener("click", animMiratanLinks);
+
+//Nuberios
+
+let animNuberiosDecoLinks = anime({
+  targets: '#nuberiosMauerLinks',
+  translateX: [
+    {value: 1000, duration: 2000},
+  ],
+  easing: 'easeOutSine',
+  loop: false,
+  autoplay: false
+});
+
+function animNuberiosLinks() {
+  animNuberiosDecoLinks.play();
+}
+
+document.getElementById('iconNuberios').addEventListener("click", animNuberiosLinks);
+
+//Thondbaran
+
+let animThondbaranDecoLinks = anime({
+  targets: '#thondbaranMauerLinks',
+  translateX: [
+    {value: 1000, duration: 2000},
+  ],
+  easing: 'easeOutSine',
+  loop: false,
+  autoplay: false
+});
+
+function animThondbaranLinks() {
+  animThondbaranDecoLinks.play();
+}
+
+document.getElementById('iconThondbaren').addEventListener("click", animThondbaranLinks);
+
+
